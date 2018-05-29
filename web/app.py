99a1d5c2d5b002c25449ba7acc7bcbfa12427a88
ablech <arek_b1993@poczta.fm>
2018-05-29 12:11:47 +0000

    project setup

--- web/app.py
+++ web/app.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
+
+message = {'value': 'Hello World'}
 
 @app.route('/')
 def hello_world():
-  return 'Hello World'
+  return jsonify(message)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5000, debug=True)
